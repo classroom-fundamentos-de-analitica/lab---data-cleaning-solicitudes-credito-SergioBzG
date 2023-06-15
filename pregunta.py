2a5ec0b0f131c477b8690024bc4e69e73b7837ba
Sergio <sebermudezg@unal.edu.co>
2023-06-14 23:47:24 -0500

    Second try

--- pregunta.py
+++ pregunta.py
@@ -1,67 +1,28 @@
 """
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
-
-Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada
+Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
 correctamente. Tenga en cuenta datos faltantes y duplicados.
-
 """
 import pandas as pd
-import re
 
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0, encoding='utf-8')
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0, encoding="utf-8")
+
     df = df.dropna()
-
-    # pd.set_option('display.max_rows', 500)
-    # print(df.barrio.value_counts())
-    df.comuna_ciudadano = df.comuna_ciudadano.astype('string').replace('.0$', '', regex=True)
-
-    # Limpieza de la columna sexo
-    df.sexo = df.sexo.str.lower()
-
-    # Limpieza de la columna tipo de emprendimiento
-    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
-
-    df.barrio = df.barrio.str.lower()
-    df.barrio = df.barrio.str.replace('[ |-]', '_', regex=True).replace('\.','', regex=True)
-
-    # Limpieza idea de negocio
-    df.idea_negocio = df.idea_negocio.map(lambda x : x.strip())
-    df.idea_negocio = df.idea_negocio.str.lower()
-    df.idea_negocio = df.idea_negocio.str.replace('[_| |-]$', '', regex=True).replace(' ', '-', regex=True).replace('_', '-', regex=True)
-    # print(df.idea_negocio.value_counts())
-
-    # Limpieza de monto del crédito
-    df.monto_del_credito = df.monto_del_credito.str.replace('\.00$', '', regex=True).replace('[\$ |,|.]', '', regex=True)
-    # print(df.monto_del_credito.value_counts())
-
-    # Limpieza de la fecha de beneficio al format día-mes-año
-    df.fecha_de_beneficio = pd.to_datetime(
-            df.fecha_de_beneficio,
-            infer_datetime_format=True,
-            errors='ignore',
-            dayfirst=True
-    )
-    df.fecha_de_beneficio = pd.to_datetime(
-            df.fecha_de_beneficio,
-            infer_datetime_format=True,
-            errors='ignore',
-            yearfirst=True
-    )
-    df.fecha_de_beneficio = df.fecha_de_beneficio.dt.strftime('%d-%m-%Y')
-
-    # Limpieza de la línea de crédito
-    df.línea_credito = df.línea_credito.str.lower()
-    df.línea_credito = df.línea_credito.map(lambda x : x.replace('-', '_').strip().replace(' ', '_'))
-    df.línea_credito = df.línea_credito.str.replace('[_| |-]$', '', regex=True)
-    # print(df.línea_credito.value_counts())
-
-    # Elimina los repetidos en todas sus columnas
+    df['sexo'] = df['sexo'].apply(str.lower)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(str.lower)
+    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ').strip() if type(x) == str else x)
+    df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ') if type(x) == str else x)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(str).apply(lambda x: x.replace('.0', ''))
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='%d/%m/%Y', errors='coerce').fillna(
+            pd.to_datetime(df['fecha_de_beneficio'], format='%Y/%m/%d', errors='coerce')
+        )
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].dt.strftime('%d-%m-%Y')
+    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: x.replace('$', '').strip().replace(',', '').replace('.00', '') if type(x) == str else x)
+    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ').strip() if type(x) == str else x)
+    df['sexo'].value_counts().to_list()
     df = df.drop_duplicates()
-
     return df
-
-clean_data()
